5167795e6e2bce26040c9a0555d62006fe88e9ab
gbrhomidi <gbrhomidi@gmail.com>
2026-06-30 02:31:12 +0300

    Replace InstrumentationRegistry with ApplicationProvider

--- app/src/androidTest/java/com/aistudio/dieselstationsms/kxmpzq/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/aistudio/dieselstationsms/kxmpzq/ExampleInstrumentedTest.kt
@@ -1,16 +1,17 @@
 package com.aistudio.dieselstationsms.kxmpzq
 
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val appContext = ApplicationProvider.getApplicationContext<MyApplication>()
         assertEquals("com.aistudio.dieselstationsms.kxmpzq", appContext.packageName)
     }
 }
